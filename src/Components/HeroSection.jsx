6b015282ee585678013c2dc73b0df182c5904352
SarbojitSaha <sarbojitsaha06012003@gmail.com>
2024-06-19 22:58:07 +0530

    update home 1st

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import IMG from '../Images/videoconfo1.png'
 import { useDisclosure } from '@chakra-ui/react'
 import { MdContentCopy } from "react-icons/md";
+import {motion} from "framer-motion";
 import {
     Modal,
     ModalOverlay,
@@ -19,10 +20,10 @@ const HeroSection = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <div>
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white">
           Transform the Way You Communicate
           </h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
@@ -49,13 +50,17 @@ const HeroSection = () => {
           </ModalFooter>
         </ModalContent>
       </Modal>
-          <a href="#/" className="inline-flex items-center justify-center border-4 px-5 py-3 text-base font-medium text-center text-gray-900  border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+          <a href="#/" className="inline-flex items-center justify-center border-4 px-5 py-3 text-base font-medium text-center text-gray-900  border-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-900 focus:ring-4 focus:ring-gray-100 text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
             Speak to Sales
           </a>
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
           <img src={IMG} alt="mockup" />
         </div>
+      
+        
+        
+       
       </div>
     </section>
     </div>
