68eff96d1722567f1ce6a89d7dd211b648e5836b
Saswata777 <saswatamaitra3.14@gmail.com>
2024-06-20 15:48:42 +0530

    Changes Responsiveness

--- src/Components/industry/Industries.jsx
+++ src/Components/industry/Industries.jsx
@@ -5,17 +5,21 @@ import { GiHealing } from "react-icons/gi";
 
 const Industries = () => {
   return (
+    <>
+    {/* parent Container*/}
     <div className='container'>
-        <div className='container left-sec '>
+        {/* left */}
+        <div className='left-sec '>
             <h1 className='ih1'>Powering Organisations acrosss <br/>
                 Industries and Geographies
             </h1>
-            <h3 className='ih3'>Zoom helps consolidate communications, connect people,
+            <h3 className='ih3 mt-10 leading-8'>Zoom helps consolidate communications, connect people,
                 and<br/> collaborate better together in the boardroom, classroom,<br/>
                 operating room, and everywhere in between
             </h3>
         </div>
-        <div className='container right-sec '>
+        {/* right */}
+        <div className='right-sec '>
             <div className='industries row '>
                 <div className='box'>
                     <IoSchool />
@@ -41,12 +45,11 @@ const Industries = () => {
                     <GiHealing /> 
                     <h2>Health</h2>
                 </div>
-                
-                
             </div>
         </div>
       
     </div>
+    </>
   )
 }
 
