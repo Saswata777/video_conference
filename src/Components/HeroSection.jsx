ffb884f2ffbe833a3a64c25ee6004c390f981ed8
Saswata777 <saswatamaitra3.14@gmail.com>
2024-06-23 17:43:05 +0530

    Update Navbar and Hero Section

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
 import IMG from '../Images/videoconfo1.png'
-import { useDisclosure } from '@chakra-ui/react'
-import { MdContentCopy } from "react-icons/md";
 import './hero.css'
+import {Link} from 'react-router-dom' 
 
-import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-    // Lorem
-  } from '@chakra-ui/react'
 
 
 const HeroSection = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    
   return (
     <div>
         <section className="bg-gray-900">
@@ -31,29 +19,16 @@ const HeroSection = () => {
                   Connect, collaborate, and conquer with our all-in-one video conferencing solution. Enjoy seamless communication and secure connections, enhancing productivity for teams everywhere.
                 </p>
                 <div className='middd'>
-                <button onClick={onOpen}  className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-slate-300 hover:text-blue-950 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900" style={{ boxShadow : "rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset"}}>
+                <Link to="/create-meeting"   className="no-underline text-current inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-slate-300 hover:text-blue-950  dark:focus:ring-primary-900" style={{ boxShadow : "rgb(204, 219, 232) 3px 3px 6px 0px inset, rgba(255, 255, 255, 0.5) -3px -3px 6px 1px inset"}}>
                   Create Meeting
                   <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" >
                     <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" ></path>
                   </svg>
-                </button>
-                <a href="#/" className="inline-flex items-center justify-center border-4 px-5 py-3 text-base font-medium text-center text-gray-900  border-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-900 focus:ring-4 focus:ring-gray-100 text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                </Link>
+                <Link to="/" className="no-underline text-current inline-flex items-center justify-center border-4 px-5 py-3 text-base font-medium text-center   border-gray-500 rounded-lg hover:bg-gray-100 hover:text-slate-800 focus:ring-4 focus:ring-gray-100 text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                   Speak to Sales
-                </a>
+                </Link>
                 </div>
-                <Modal onClose={onClose} isOpen={isOpen} isCentered>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                  <div className=' flex'> Meet Link  <MdContentCopy /></div>
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button onClick={onClose}>Close</Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
               </div>
               <div className=" lg:mt-0 lg:col-span-5 lg:flex">
                 <img src={IMG} alt="mockup" />
