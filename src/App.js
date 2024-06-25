035a3432514926f34b4cb7f50246d1b2ceac6bb3
Saswata777 <saswatamaitra3.14@gmail.com>
2024-06-25 08:48:30 +0530

    Integrate create Meeting

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import LogInForm from './Components/LogInForm';
 import SignUp from './Pages/SignUp';
 import Joinn from './Components/Joinn';
 import AboutPage from './Pages/AboutPage';
+import CreateMeeting from './Pages/createMeeting';
+import VideoCallPage from './Pages/VideoCallPage';
 function App() {
   return (
     <div className="App">
@@ -16,6 +18,8 @@ function App() {
         <Route path='/signup' element={<SignUp/>} />
         <Route path='/joinn' element={<Joinn/>} /> 
         <Route path='/about' element={<AboutPage/>} /> 
+        <Route path="/create-meeting" element={<CreateMeeting />} />
+        <Route path="/room/:roomID" element={< VideoCallPage/>} />
       </Routes>
       </BrowserRouter>
       
