d1d79242e8d200341fed56b7e565ca9badc649c0
soham mondal <sohammondal50285@gmail.com>
2024-06-25 11:49:34 +0530

    contact us page

---
+++ src/Pages/ContactPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Contact from '../Components/Contact'
+import Foooter from '../Components/Foooter'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Contact/>
+      <Foooter/>
+    </div>
+  )
+}
+
+export default ContactPage
