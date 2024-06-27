ecc8de0afd5fd849430a4d84508604d75b4fd70d
SarbojitSaha <sarbojitsaha06012003@gmail.com>
2024-06-27 19:31:36 +0530

    files arranged in admin

---
+++ src/Pages/AdminUser.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../Components/admin/Sidebar'
+import User from '../Components/admin/user'
+
+
+const AdminUser = () => {
+  return (
+    <div>
+      <Sidebar/>
+      <User/>
+    </div>
+  )
+}
+
+export default AdminUser
