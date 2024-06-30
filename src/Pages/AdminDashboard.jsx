1f74b598dac1208460ce64a80554a86d092e403f
SarbojitSaha <sarbojitsaha06012003@gmail.com>
2024-06-30 20:20:32 +0530

    dash cards

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from '../Components/admin/Sidebar'
 import Foooter from '../Components/Foooter'
+import Dashboard from '../Components/admin/Dashboard'
 
 const AdminDashboard = () => {
   return (
     <div >
       <div className='flex'>
       <Sidebar/>
+      <Dashboard/>
       </div>
       
       <Foooter/>
