6a72647b7ee88dce92d242561498721cbc4c3010
Saswata777 <saswatamaitra3.14@gmail.com>
2024-07-02 20:37:42 +0530

    Modify index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import { Connection } from './database/db.js';
 const app = express();
+app.get('/',(req, res)=>{
+    res.send("This is Home page");
+})
 Connection();
 const PORT = 8000;
 app.listen(PORT,()=>{
