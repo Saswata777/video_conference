6dca0a956080e267ea2d1a674d71b20593e0d191
SarbojitSaha <sarbojitsaha06012003@gmail.com>
2024-07-02 21:46:33 +0530

    usestate login

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ const URI = "mongodb+srv://upmeet2024:izmR37vjMAoC2U1f@cluster0.oz8gfkj.mongodb.
 
 export const Connection = async()=>{
     try {
-         await mongoose.connect(URI);
+        await mongoose.connect(URI);
         console.log("MongoDB Data base is connected");
     } catch (error) {
         console.log("Unable to Connect MongoDb data base ", error);
